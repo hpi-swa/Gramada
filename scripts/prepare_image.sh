446c11aa4fe028c9270c329dcb5221a880b1316f
Patrick R <patrick.rein@gmail.com>
2017-05-20 20:10:53 +0200

    Updates build script

--- scripts/prepare_image.sh
+++ scripts/prepare_image.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2014-2016 Software Architecture Group (Hasso Plattner Institute)
+# Copyright (c) 2015 Fabio Niephaus, Google Inc.
+# Copyright (c) 2017 Patrick Rein
 
 set -e
 
@@ -6,38 +9,46 @@ function print_info {
     printf "\e[0;34m$1\e[0m\n"
 }
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-    print_info "Nothing to do, because this is not the master branch"
+# Check required arguments
+# ==============================================================================
+if [[ "${TRAVIS_BRANCH}" != "master" ]] || [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    print_info "Nothing to do, because this is not the master branch or a PR."
     exit 0
+elif [[ "${TRAVIS_OS_NAME}" != "linux" ]]; then
+    print_info "Nothing to do, because this is not a Linux build."
+    exit 0
+elif [[ -z "${TRAVIS_BUILD_DIR}" ]]; then
+    print_info "\$TRAVIS_BUILD_DIR is not defined!"
+    exit 1
+elif [[ -z "${SMALLTALK_CI_HOME}" ]]; then
+    print_info "\$TRAVIS_SMALLTALK_VERSION_CI_HOME is not defined!"
+    exit 1
+elif [[ -z "${TRAVIS_SMALLTALK_VERSION}" ]]; then
+    print_info "\$TRAVIS_SMALLTALK_VERSION is not defined!"
+    exit 1
 fi
 
-if [ -z "$PROJECT_HOME" ]; then
-    print_info "\$PROJECT_HOME is not defined!"
-    exit 1
-elif [ -z "$FILETREE_CI_HOME" ]; then
-    print_info "\$FILETREE_CI_HOME is not defined!"
-    exit 1
-elif [ -z "$SMALLTALK" ]; then
-    print_info "\$SMALLTALK is not defined!"
-    exit 1
-fi
+# ==============================================================================
 
-DEPLOY_PATH="$FILETREE_CI_HOME/deploy"
-
-BASE_PATH="$FILETREE_CI_HOME"
-CACHE_PATH="$BASE_PATH/cache"
-VM_PATH="$CACHE_PATH/vms"
-COG_VM_PATH="$VM_PATH/coglinux/bin/squeak"
-COG_VM_PARAM="-nosound -nodisplay"
+# Set paths and files
+# ==============================================================================
+VIVIDE_IMAGE="Vivide-${TRAVIS_SMALLTALK_VERSION}.image"
+VIVIDE_CHANGES="Vivide-${TRAVIS_SMALLTALK_VERSION}.changes"
 VIVIDE_SOURCE_URL="https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/vivide/"
-VIVIDE_IMAGE="Vivide-$SMALLTALK.image"
-VIVIDE_CHANGES="Vivide-$SMALLTALK.changes"
-DEPLOY_TARGET="https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/gramada/"
-GRAMADA_IMAGE="Gramada-$SMALLTALK.image"
-GRAMADA_Changes="Gramada-$SMALLTALK.changes"
 
-mkdir "$DEPLOY_PATH"
-cd "$DEPLOY_PATH"
+GRAMADA_IMAGE="Gramada-${TRAVIS_SMALLTALK_VERSION}.image"
+GRAMADA_Changes="Gramada-${TRAVIS_SMALLTALK_VERSION}.changes"
+
+DEPLOY_PATH="${SMALLTALK_CI_HOME}/deploy"
+DEPLOY_TARGET="https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/gramada/"
+
+COG_VM_PARAM=""
+if [[ "$(uname -s)" == "Linux" ]]; then
+    COG_VM_PARAM="-nosound -nodisplay"
+fi
+# ==============================================================================
+
+mkdir "${DEPLOY_PATH}" && cd "${DEPLOY_PATH}
 
 print_info "Downloading Vivide image"
 wget "${VIVIDE_SOURCE_URL}\\${VIVIDE_IMAGE}"
@@ -45,11 +56,11 @@ wget "${VIVIDE_SOURCE_URL}\\${VIVIDE_CHANGES}"
 mv *.image "$GRAMADA_IMAGE"
 mv *.changes "$GRAMADA_CHANGES"
 
-if [ $SMALLTALK == "Squeak4.6" ]; then
+if [[ $TRAVIS_SMALLTALK_VERSION == "Squeak4.6" ]]; then
     print_info "Downloading Squeak4.6 sources..."
     wget http://ftp.squeak.org/sources_files/SqueakV46.sources.gz
     gunzip SqueakV46.sources.gz
-elif [ $SMALLTALK == "Squeak5.1" ]; then
+elif [[ $TRAVIS_SMALLTALK_VERSION == "Squeak5.1" ]]; then
     print_info "Downloading Squeak5.1 sources..."
     wget http://ftp.squeak.org/sources_files/SqueakV50.sources.gz
     gunzip SqueakV50.sources.gz
@@ -59,7 +70,7 @@ else
     gunzip SqueakV60.sources.gz
 fi
 
-print_info "Preparing Gramada image from $SMALLTALK Vivide image..."
+print_info "Preparing Gramada image from {$TRAVIS_SMALLTALK_VERSION} Vivide image..."
 EXIT_STATUS=0
 "$COG_VM_PATH" $COG_VM_PARAM "$GRAMADA_IMAGE" "$PROJECT_HOME/scripts/prepare_image.st" || EXIT_STATUS=$?
 
